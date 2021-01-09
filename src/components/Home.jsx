d48709a13ad9e01c54907bd8f39e88f11af4cabf
Justin Kyle Chang <justinklchang@yahoo.com>
2021-01-08 21:56:23 -0800

    Added responsive nav bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../css/Home.css'
 import homeProfile from '../assets/images/home-profile.png'
 import navLogo from '../assets/images/portfolio_logo_black.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar (props) {
+    const [menu, toggleMenu] = useState(false);
+    const onClick = () => toggleMenu(!menu);
+
     return (
-        <div id="navbar-container" className="width-size center">
+        <div id="navbar-container" className="center width-size">
             <nav id="navbar-contents">
                 <ul key="About"><a href='/#about'>About</a></ul>
                 <ul key="Contact"><a href='/#contact'>Contact</a></ul>
@@ -13,6 +18,21 @@ function NavBar (props) {
                 <ul key="Projects"><a href='/#projects'>Projects</a></ul>
                 <ul key="Hire"><a href='/#hire'>Hire Me</a></ul>
             </nav>
+            { menu ?
+                <FontAwesomeIcon icon={faTimes} id="navbar-menu-button" onClick={onClick}/>:
+                <FontAwesomeIcon icon={faBars} id="navbar-menu-button" onClick={onClick}/>
+            }
+            <nav id="navbar-menu-content" className={menu ? "active" : null}>
+                <ul key="About"><a href='/#about'>About</a></ul>
+                <hr></hr>
+                <ul key="Contact"><a href='/#contact'>Contact</a></ul>
+                <hr></hr>
+                <ul key="Projects"><a href='/#projects'>Projects</a></ul>
+                <hr></hr>
+                <ul key="Hire"><a href='/#hire'>Hire Me</a></ul>
+                <hr></hr>
+            </nav>
+            { menu ? <div className="white-overlay"></div> : null }
         </div>
     )
 }
